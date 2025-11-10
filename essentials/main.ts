72c9800835fa039f98b8646c783e6ade5ed41bc9
VictorAndres20 <vapedraza1706@gmail.com>
2025-11-10 18:16:18 -0500

    chore: uncomment nest main.ts comments

--- essentials/main.ts
+++ essentials/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
-// import * as bodyParser from 'body-parser';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,9 +12,9 @@ async function bootstrap() {
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
-  // app.use(bodyParser.json({ limit: '100mb' }));
-  // app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
-  // app.setGlobalPrefix('api');
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  app.setGlobalPrefix('api');
   await app.listen(Number(process.env.SERVER_PORT));
 }
 bootstrap();
