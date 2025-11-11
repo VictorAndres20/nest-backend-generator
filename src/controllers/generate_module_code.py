a720d71b22ac02e282bced8e34ea2da8d78ee074
VictorAndres20 <vapedraza1706@gmail.com>
2025-11-10 20:17:40 -0500

    chore: constants to store generated paths

--- src/controllers/generate_module_code.py
+++ src/controllers/generate_module_code.py
@@ -19,25 +19,39 @@ from src.helpers.folder_handler import create_folder, copy_essentials, copy_reac
 from src.helpers.write_file import write_code
 
 
+NEST_ROOT_PATH = "nest"
+NEST_SRC_PATH = f"{NEST_ROOT_PATH}/src"
+NEST_SRC_API_PATH = f"{NEST_SRC_PATH}/api"
+NEST_SRC_ASSETS_PATH = f"{NEST_SRC_PATH}/assets"
+
+DB_PATH = "db"
+
+REACT_JS_ROOT_PATH = "react-js"
+REACT_JS_SRC_PATH = f"{REACT_JS_ROOT_PATH}/src"
+REACT_JS_SRC_SERVICES_PATH = f"{REACT_JS_SRC_PATH}/_services"
+REACT_JS_SRC_EVENTS_PATH = f"{REACT_JS_SRC_PATH}/_events"
+REACT_JS_SRC_HOOKS_PATH = f"{REACT_JS_SRC_PATH}/_hooks"
+
+
 def generate_module(models_path: str, file_path: str, db_schema: str, generate_sql_relations: bool = False):
 
     # Create base folders
 
-    create_folder(models_path + "nest")
-    create_folder(models_path + "nest/src")
-    create_folder(models_path + "nest/src/api")
-    create_folder(models_path + "nest/src/assets")
-    create_empty_file(models_path + "nest/src/assets/.gitkeep")
+    create_folder(models_path + NEST_ROOT_PATH)
+    create_folder(models_path + NEST_SRC_PATH)
+    create_folder(models_path + NEST_SRC_API_PATH)
+    create_folder(models_path + NEST_SRC_ASSETS_PATH)
+    create_empty_file(models_path + NEST_SRC_ASSETS_PATH + "/.gitkeep")
 
-    create_folder(models_path + "db")
+    create_folder(models_path + DB_PATH)
 
-    create_folder(models_path + "react")
-    create_folder(models_path + "react/src")
-    create_folder(models_path + "react/src/_services")
-    create_folder(models_path + "react/src/_events")
+    create_folder(models_path + REACT_JS_ROOT_PATH)
+    create_folder(models_path + REACT_JS_SRC_PATH)
+    create_folder(models_path + REACT_JS_SRC_SERVICES_PATH)
+    create_folder(models_path + REACT_JS_SRC_EVENTS_PATH)
 
     # First copy React
-    copy_react(models_path + "react/src/")
+    copy_react(models_path + f"{REACT_JS_SRC_PATH}/")
 
     ddl = ''
 
@@ -74,38 +88,38 @@ def generate_module(models_path: str, file_path: str, db_schema: str, generate_s
 
         # Create Nest entity folders
 
-        create_folder(models_path + "nest/src/api/" + module_name)
-        create_folder(models_path + "nest/src/api/" + module_name + "/entity")
-        create_folder(models_path + "nest/src/api/" + module_name + "/service")
-        create_folder(models_path + "nest/src/api/" + module_name + "/controller")
+        create_folder(models_path + f"{NEST_SRC_API_PATH}/" + module_name)
+        create_folder(models_path + f"{NEST_SRC_API_PATH}/" + module_name + "/entity")
+        create_folder(models_path + f"{NEST_SRC_API_PATH}/" + module_name + "/service")
+        create_folder(models_path + f"{NEST_SRC_API_PATH}/" + module_name + "/controller")
 
         # Generate Nest entity with DTO
 
         entity_generator.clean()
         entity_generator.build_class(list_attr)
-        write_code(models_path + "nest/src/api/" + module_name + "/entity/" + module_name + ".entity.ts",
+        write_code(models_path + f"{NEST_SRC_API_PATH}/" + module_name + "/entity/" + module_name + ".entity.ts",
                    entity_generator.content, False)
-        write_code(models_path + "nest/src/api/" + module_name + "/entity/" + module_name + ".dto.ts",
+        write_code(models_path + f"{NEST_SRC_API_PATH}/" + module_name + "/entity/" + module_name + ".dto.ts",
                    entity_generator.dto_content, False)
 
         # Generate and write Nest service
 
         service_generator.clean()
         service_generator.build_class(module_name, class_dict, pk_dict, list_attr)
-        write_code(models_path + "nest/src/api/" + module_name + "/service/" + module_name + ".service.ts",
+        write_code(models_path + f"{NEST_SRC_API_PATH}/" + module_name + "/service/" + module_name + ".service.ts",
                    service_generator.content, False)
 
         # Generate and write Nest business file
         business_generator.clean()
         business_generator.build_class(module_name, class_dict, pk_dict, list_attr)
-        write_code(models_path + "nest/src/api/" + module_name + "/service/" + module_name + ".business.ts",
+        write_code(models_path + f"{NEST_SRC_API_PATH}/" + module_name + "/service/" + module_name + ".business.ts",
                    business_generator.content, False)
 
         # Generate and write Nest controller
 
         controller_generator.clean()
         controller_generator.build_class(module_name, class_dict, pk_dict)
-        write_code(models_path + "nest/src/api/" + module_name + "/controller/" + module_name + ".controller.ts",
+        write_code(models_path + f"{NEST_SRC_API_PATH}/" + module_name + "/controller/" + module_name + ".controller.ts",
                    controller_generator.content, False)
 
         # Generate and write Nest Module
@@ -113,7 +127,7 @@ def generate_module(models_path: str, file_path: str, db_schema: str, generate_s
         entity_module_generator = EntityModuleGenerator(class_dict["name"])
         entity_module_generator.build(module_name, class_dict["name"])
         entity_module_generator.build_class()
-        write_code(models_path + "nest/src/api/" + module_name + "/" + module_name + ".module.ts",
+        write_code(models_path + f"{NEST_SRC_API_PATH}/" + module_name + "/" + module_name + ".module.ts",
                    entity_module_generator.content, False)
 
         # Generate DB DDL code
@@ -125,37 +139,37 @@ def generate_module(models_path: str, file_path: str, db_schema: str, generate_s
         # Generate and write React JS entity service
         react_service_generator.clean()
         react_service_generator.build_class(module_name, class_dict, pk_dict)
-        write_code(models_path + "react/src/_services/" + module_name + ".service.js",
+        write_code(models_path + f"{REACT_JS_SRC_SERVICES_PATH}/" + module_name + ".service.js",
                    react_service_generator.content, False)
 
         # Create React JS event folder
 
-        create_folder(models_path + "react/src/_events/" + module_name)
+        create_folder(models_path + f"{REACT_JS_SRC_EVENTS_PATH}/" + module_name)
 
         # Generate and write React JS entity model
 
         react_model_generator.clean()
         react_model_generator.build_class(list_attr)
-        write_code(models_path + "react/src/_events/" + module_name + "/model.js",
+        write_code(models_path + f"{REACT_JS_SRC_EVENTS_PATH}/" + module_name + "/model.js",
                    react_model_generator.content, False)
 
         # Generate and write React JS entity find event
 
         react_find_event_generator.clean()
         react_find_event_generator.build_class(module_name, class_dict)
-        write_code(models_path + "react/src/_events/" + module_name + "/find.event.js",
+        write_code(models_path + f"{REACT_JS_SRC_EVENTS_PATH}/" + module_name + "/find.event.js",
                    react_find_event_generator.content, False)
 
         # Generate and write React JS entity create event
 
         react_create_event_generator.clean()
         react_create_event_generator.build_class(module_name, class_dict)
-        write_code(models_path + "react/src/_events/" + module_name + "/create.event.js",
+        write_code(models_path + f"{REACT_JS_SRC_EVENTS_PATH}/" + module_name + "/create.event.js",
                    react_create_event_generator.content, False)
 
         # Create React JS hooks folder
 
-        create_folder(models_path + "react/src/_hooks/" + module_name)
+        create_folder(models_path + f"{REACT_JS_SRC_HOOKS_PATH}/" + module_name)
 
     # ######### After iterate all entities operations:
 
@@ -164,19 +178,19 @@ def generate_module(models_path: str, file_path: str, db_schema: str, generate_s
     api_module_generator = ApiModuleGenerator()
     api_module_generator.build(modules)
     api_module_generator.build_class()
-    write_code(models_path + "nest/src/api/api.module.ts", api_module_generator.content, False)
+    write_code(models_path + f"{NEST_SRC_API_PATH}/api.module.ts", api_module_generator.content, False)
 
     # Generate and write Nest index.ts for modules exports
 
     index_api_generator = IndexApiGenerator(module_names)
     index_api_generator.build_class()
-    write_code(models_path + "nest/src/api/index.ts", index_api_generator.content, False)
+    write_code(models_path + f"{NEST_SRC_API_PATH}/index.ts", index_api_generator.content, False)
 
     # Copy Nest essentials
 
-    copy_essentials(models_path + "nest/src/")
-    copy_essential_files(models_path + "nest/src/")
-    copy_nest_essential_env_files(models_path + "nest/")
+    copy_essentials(models_path + f"{NEST_SRC_PATH}/")
+    copy_essential_files(models_path + f"{NEST_SRC_PATH}/")
+    copy_nest_essential_env_files(models_path + f"{NEST_ROOT_PATH}/")
 
     # Generate DB relations
 
@@ -194,9 +208,9 @@ def generate_module(models_path: str, file_path: str, db_schema: str, generate_s
 
     # Write DB DDL
 
-    write_code(models_path + "db/ddl.sql", ddl, True)
+    write_code(models_path + f"{DB_PATH}/ddl.sql", ddl, True)
 
     # Copy React JS essentials
 
-    copy_react_essential_files(models_path + "react/src/_services/")
-    copy_react_essential_appjs_files(models_path + "react/src/")
+    copy_react_essential_files(models_path + f"{REACT_JS_SRC_SERVICES_PATH}/")
+    copy_react_essential_appjs_files(models_path + f"{REACT_JS_SRC_PATH}/")
