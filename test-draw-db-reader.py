4b19e423f6cede390cb08afbfa37f2afd9f80847
VictorAndres20 <vapedraza1706@gmail.com>
2025-11-15 18:14:01 -0500

    fix: imports in test drawdb reader

--- test-draw-db-reader.py
+++ test-draw-db-reader.py
@@ -2,7 +2,7 @@ import os
 from src.helpers.drawdb_reader import build_list_modules_from_draw_db_io
 from dotenv import dotenv_values
 
-config = dotenv_values("../../.env")
+config = dotenv_values(".env")
 
 model_file_path = config["MODELS_INPUT_PATH"]
 model_file_name = config["MODELS_FILE_NAME"]
