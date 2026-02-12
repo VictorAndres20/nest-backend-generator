414156541482b2a22d7635622b89b257169a6322
VictorAndres20 <vapedraza1706@gmail.com>
2026-02-12 17:12:36 -0500

    fix: api config URL

--- react-ts/_config/api.ts
+++ react-ts/_config/api.ts
@@ -2,7 +2,7 @@
 export const API_URL = import.meta.env.VITE_API_URL;
 
 // Set here api prefix paths if they are defined in environment variables
-export const API_URL_PREFIX_PATH = import.meta.env.VITE_API_URL_PREFIX;
+export const API_URL_PREFIX_PATH = import.meta.env.VITE_API_URL_PREFIX ?? "";
 
 // Construct full API host URLs to use in api calls
 export const MAIN_API_HOST = `${API_URL}${API_URL_PREFIX_PATH}`;
