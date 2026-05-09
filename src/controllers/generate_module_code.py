b4a9d4eb72d9fe4112ee2ded83e805eb5d278640
victorandres20 <vapedraza1706@gmail.com>
2026-05-09 18:18:19 -0500

    chore: decoplelize dtos

--- src/controllers/generate_module_code.py
+++ src/controllers/generate_module_code.py
@@ -209,6 +209,7 @@ def generate_module(models_path: str, file_path: str, db_schema: str, generate_s
         # Create Nest entity folders
 
         create_folder(models_path + f"{NEST_SRC_API_PATH}/" + module_name)
+        create_folder(models_path + f"{NEST_SRC_API_PATH}/" + module_name + "/models")
         create_folder(models_path + f"{NEST_SRC_API_PATH}/" + module_name + "/service")
         create_folder(models_path + f"{NEST_SRC_API_PATH}/" + module_name + "/controller")
 
@@ -220,6 +221,8 @@ def generate_module(models_path: str, file_path: str, db_schema: str, generate_s
                    entity_generator.content, False)
         write_code(models_path + f"{NEST_SRC_MODELS_PATH}/" + module_name + ".dto.ts",
                    entity_generator.dto_content, False)
+        write_code(models_path + f"{NEST_SRC_API_PATH}/" + module_name + "/models/" + module_name + ".dto.ts",
+                   f"export * from '../../../{NEST_MODELS_DIR_NAME}/" + module_name + ".dto.ts'", False)
 
         # Generate and write Nest service
 
